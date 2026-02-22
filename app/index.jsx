e4719b292176f95a9ff05ddccb90e66785cd78f4
Abdullah's MacBook <apple@APPLEs-MacBook-Pro.local>
2026-02-22 14:43:22 +0500

    Lecture 1: Expo setup and Appwrite configuration complete

---
+++ app/index.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <View style={{ paddingTop: 50, paddingLeft: 20 }}>
+      <Text style={{ fontSize: 24, fontWeight: 'bold' }}>Abdullah</Text>
+    </View>
+  )
+}
+
+export default Home
+
+const styles = StyleSheet.create({})
